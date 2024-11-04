be1ec6459d06294db91b3306720741bb558c10eb
Edwin Laboy <63374885+elaboy@users.noreply.github.com>
2024-11-04 16:38:58 -0600

    dice done

--- Dicee+Challenge+-+Starting+Files/index.js
+++ Dicee+Challenge+-+Starting+Files/index.js
@@ -1,1 +1,19 @@
-var randomNumber1 = Math.random() + 5
+function randomNumberGenerator(){
+    var randomNumber = Math.floor(Math.random() * 6) + 1;
+    return randomNumber;
+}
+var leftDice = randomNumberGenerator();
+var rightDice = randomNumberGenerator();
+
+document.querySelector(".img1").setAttribute("src", "./images/dice"+leftDice+".png");
+document.querySelector(".img2").setAttribute("src", "./images/dice"+rightDice+".png");
+
+if(leftDice > rightDice){
+    document.querySelector("h1").textContent = "Player 1 Wins!";
+}
+else if(rightDice > leftDice){
+    document.querySelector("h1").textContent = "Player 2 Wins!";
+}
+else if(rightDice === leftDice){
+    document.querySelector("h1").textContent = "Draw!"
+}
